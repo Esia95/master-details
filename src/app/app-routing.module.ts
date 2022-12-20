3bca6b43d66c501b14feda3eb3fc470bdb67d205
Estera Nowak <esteranowak95@gmail.com>
2022-12-20 16:24:21 +0100

    feat: list of cryptocurrency implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { EmployeeComponent } from './components/employee/employee.component';
+import { CryptoComponent } from './components/crypto/crypto.component';
 import { ProductListComponentModule } from './components/product-list/product-list.component-module';
 import { ProductServiceModule } from './services/product.service-module';
 import { EmployeeComponentModule } from './components/employee/employee.component-module';
 import { EmployeeServiceModule } from './services/employee.service-module';
+import { CryptoComponentModule } from './components/crypto/crypto.component-module';
+import { CryptoServiceModule } from './services/crypto.service-module';
 
 @NgModule({
-  imports: [RouterModule.forRoot([{ path: 'products-master-details', component: ProductListComponent }, { path: 'employees-master-details', component: EmployeeComponent }]), ProductListComponentModule, ProductServiceModule, EmployeeComponentModule, EmployeeServiceModule],
+  imports: [RouterModule.forRoot([{ path: 'products-master-details', component: ProductListComponent }, { path: 'employees-master-details', component: EmployeeComponent }, { path: 'crypto-master-details', component: CryptoComponent }]), ProductListComponentModule, ProductServiceModule, EmployeeComponentModule, EmployeeServiceModule, CryptoComponentModule, CryptoServiceModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/app/components/crypto/crypto.component.ts
@@ -0,0 +1,25 @@
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { CryptoModel } from '../../models/crypto.model';
+import { CryptoService } from '../../services/crypto.service';
+
+@Component({
+  selector: 'app-crypto',
+  styleUrls: ['./crypto.component.scss'],
+  templateUrl: './crypto.component.html',
+  encapsulation: ViewEncapsulation.Emulated,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class CryptoComponent {
+  readonly list$: Observable<CryptoModel[]> = this._cryptoService.getAll();
+
+  private _detailsSubject: Subject<CryptoModel> = new Subject<CryptoModel>();
+  public details$: Observable<CryptoModel> = this._detailsSubject.asObservable();
+
+  constructor(private _cryptoService: CryptoService) {
+  }
+
+  selectCrypto(crypto: CryptoModel): void {
+    this._detailsSubject.next(crypto)
+  }
+}
